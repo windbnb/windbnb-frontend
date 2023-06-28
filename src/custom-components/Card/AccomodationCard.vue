0f17b7a8797eb39f41ee41778c9fc9164777f74b
nikolinat <nikolinatosic999@gmail.com>
2023-06-28 18:20:54 +0200

    feat: changed props in StarRating

--- src/custom-components/Card/AccomodationCard.vue
+++ src/custom-components/Card/AccomodationCard.vue
@@ -1,14 +1,14 @@
 <template>
  <div>
-        <div class="card card-chart" v-if="fetchedAccomodationData.displayImage !== null  && fetchedAccomodationData.average !== 0">
-            <div class="card-header card-header">
-                <img :src="fetchedAccomodationData.displayImage" id="image" @load="load" style="height: 200px;max-width: 100%;"/>
+        <div class="card card-chart">
+            <div :v-if="image">
+                <img :src="image" id="image" style="height: 200px;max-width: 100%;"/>
             </div>
-            <div class="card-body">
+            <div class="card-body" :v-if="fetchedAccomodation">
                 <!-- <h4 class="card-title">{{fetchedAccomodationData}}</h4> -->
-                <p class="card-title">{{fetchedAccomodationData.accomodation.address}}</p>
-               <p class="card-title">{{fetchedAccomodationData.average}}</p>
-                <StarRating v-model="fetchedAccomodationData.average" :read-only=true></StarRating>
+                <p class="card-title">{{fetchedAccomodation.accomodation.address}}</p>
+               <p class="card-title">{{fetchedAccomodation.average}}</p>
+                <StarRating :rating="averageRate" :read-only=true></StarRating>
             </div>
             <div class="card-footer">
                 <div class="stats">
@@ -33,57 +33,57 @@ export default {
         fetchedAccomodation: {}
     },
 
-data: function () {
-    return {
-      image: null,
-      fetchedAccomodationData: {
-        accomodation: {
-            address: ""
+    data: function () {
+        return {
+        image: null,
+        averageRate: 0
         }
-      }
-    };
-},
-computed: {
-    ...mapGetters({
-      fetchedImage: "accomodation/getImage",
-      averageAccomodation: "review/getAverageAccomodation"
+    },
+    computed: {
+        ...mapGetters({
+            fetchedImage: "accomodation/getImage",
+            averageAccomodation: "review/getAverageAccomodation"
+                }),
+        },
+
+    watch: {
+        fetchedImage(newImage) {
+            this.image = "data:image/jpg;base64," + newImage;
+            this.fetchedAccomodation.displayImage = "data:image/jpg;base64," + newImage;
+        },
+
+        averageAccomodation(newAverageAccomodation){
+            console.log(newAverageAccomodation)
+            this.fetchedAccomodation.average = newAverageAccomodation.AverageRaiting;
+            this.averageRate = newAverageAccomodation.AverageRaiting;
+            // this.$set(this, "fetchedAccomodationData", newAverageAccomodation)
+            // this.$set(this.fetchedAccomodationData, "average", newAverageAccomodation.AverageRaiting)
+        }
+
+    },
+
+    methods: {
+            ...mapActions({
+            fetchImage: "accomodation/fetchImage",
+            fetchAverageAccomodation: "review/fetchAverageAccomodation"
         }),
+
+        load(){
+            URL.revokeObjectURL(this.fetchedAccomodation.displayImage);
+            document.getElementById('image').src = this.image;
+        },
+
+        viewMore(){
+            this.$router.push({name:'AccomodationPage', path: '/accomodation/'+ this.fetchedAccomodationData.accomodation.id, params: { accomodation: this.fetchedAccomodation }});
+        }
     },
 
-watch: {
-     fetchedImage(newImage) {
-        this.fetchedAccomodationData.displayImage = newImage;
+    mounted() {
+        console.log(this.fetchedAccomodation)
+        this.fetchedAccomodationData = this.fetchedAccomodation;
+        this.fetchImage(this.fetchedAccomodationData.accomodation.images[0])
+        this.fetchAverageAccomodation(this.fetchedAccomodationData.accomodation.id)
     },
-
-    averageAccomodation(newAverageAccomodation){
-        this.fetchedAccomodationData.average = newAverageAccomodation.AverageRaiting;
-        this.$set(this, "fetchedAccomodationData", newAverageAccomodation)
-        this.$set(this.fetchedAccomodationData, "average", newAverageAccomodation.AverageRaiting)
-    }
-
-},
-
-methods: {
-        ...mapActions({
-        fetchImage: "accomodation/fetchImage",
-        fetchAverageAccomodation: "review/fetchAverageAccomodation"
-    }),
-
-    load(){
-        URL.revokeObjectURL(this.fetchedAccomodation.displayImage);
-        document.getElementById('image').src = this.fetchedAccomodation.displayImage;
-    },
-
-    viewMore(){
-        this.$router.push({name:'AccomodationPage', path: '/accomodation/'+ this.fetchedAccomodationData.accomodation.id, params: { accomodation: this.fetchedAccomodation }});
-    }
-},
-
-mounted() {
-    this.fetchedAccomodationData = this.fetchedAccomodation;
-    this.fetchImage(this.fetchedAccomodationData.accomodation.images[0])
-    this.fetchAverageAccomodation(this.fetchedAccomodationData.accomodation.id)
-},
 
 }
 
